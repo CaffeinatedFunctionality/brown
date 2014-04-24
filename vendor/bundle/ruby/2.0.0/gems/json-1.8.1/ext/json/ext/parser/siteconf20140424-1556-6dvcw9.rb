8e73e4b4be6a8ccc405872943bd8555a2fc80c1d
Taylor Mitchell <scy0846@gmail.com>
2014-04-24 01:22:58 -0400

    heroku

---
+++ vendor/bundle/ruby/2.0.0/gems/json-1.8.1/ext/json/ext/parser/siteconf20140424-1556-6dvcw9.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140424-1556-cm4pul"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path

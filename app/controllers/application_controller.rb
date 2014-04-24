ba1d070b77883e659fe3a8cd767a62cc2c59c907
Taylor Mitchell <scy0846@gmail.com>
2014-04-24 13:56:33 -0400

    added proper text, some formatting. Todo:setup actionmailer and minor touchups

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :check_for_mobile
+
+  private
+
+  def mobile_device?
+    if session[:mobile_override]
+      session[:mobile_override] == "1"
+    else
+      # Season this regexp to taste. I prefer to treat iPad as non-mobile.
+      (request.user_agent =~ /(iPhone|iPod|Android|webOS|Mobile)/) && (request.user_agent !~ /iPad/)
+    end
+  end
+  helper_method :mobile_device?
+
+  def check_for_mobile
+    session[:mobile_override] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
+  end
+
+  
+  
+
 end

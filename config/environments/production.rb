da27f13c8a8d15e3d30731594e273402638fc8ab
Taylor Mitchell <scy0846@gmail.com>
2014-04-24 20:38:15 -0400

    testing for mobile site

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,7 @@ Brown::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.compress = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true

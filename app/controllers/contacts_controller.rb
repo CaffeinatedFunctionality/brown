1794f2d3f8a9837d5d4d0a55fb57c17e6026f761
Taylor Mitchell <scy0846@gmail.com>
2014-04-26 15:45:35 -0400

    mailer set up

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      redirect_to(root_path, :notice => "Thank you for contacting me. I will reply shortly!")
     else
       flash.now[:error] = 'Cannot send message.'
       render :new

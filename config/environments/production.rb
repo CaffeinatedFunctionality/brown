3ab40b73970530d0ce22e75589dbc2cfa17f5eaf
Taylor Mitchell <scy0846@gmail.com>
2014-04-30 11:14:08 -0400

    mobile testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Brown::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_options = { :host => 'yoursite.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'brown2014.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true

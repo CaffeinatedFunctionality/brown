d4a01d3726da3e75c917fe9376323c7ba4f7d35e
Taylor Mitchell <scy0846@gmail.com>
2014-05-01 09:27:37 -0400

    mobile testing

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.delivery_method = :smtp
+
 # Initialize the Rails application.
 Brown::Application.initialize!
